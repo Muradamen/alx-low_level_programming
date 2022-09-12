40043baa948d67b37019421bb08e4ec72ac358d8
Muradamen <muradamen10@gmail.com>
2022-09-12 00:57:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return:Always 0(Success)
+ */
+int main(void)
+{     puts("\"Programming is like biulding a multilingual puzzle");
+	return(0)
+
+}
