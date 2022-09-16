dd99f587c00a1f5028dfb73dbc3262cfa3231436
Muradamen <muradamen10@gmail.com>
2022-09-16 06:20:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,13 @@
-#include "holberton.h"
-/**
- * main - prints the string "Holberton" from a character array.
- * Return: 0 on success.
- */
-int main(void)
-{
-	char c[] = "Holberton";
-	int i;
+#include <unistd.h>
 
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
 }
