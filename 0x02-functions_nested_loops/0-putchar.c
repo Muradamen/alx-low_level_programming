bcdfec3e34028e068bd7f39f3bdb23666de35b24
Muradamen <muradamen10@gmail.com>
2022-09-16 05:41:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,17 @@
-/*
- * File:  0-putchar.c
- * Auth: @Muradamen
- *
- */
-
-#include <stio.h>
-
+#include "holberton.h"
 /**
- * main - Prints  followed by a new line.
- *
- * Return: Always 0.
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char c[] = "Holberton";
+	int i;
 
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
