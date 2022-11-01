c3496a0ae0fde0d20a7ee4a13ddc13a8535da72a
Muradamen <muradamen10@gmail.com>
2022-11-01 22:21:01 +0300

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,36 +1,50 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <stddef.h>
-#include <string.h>
-/**
- * append_text_to_file - Appends to a file
- * @filename: Filename
- * @text_content: String to write to file
- * Return: 1 for success, -1 on failure
- */
 
+/**
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
+ *
+ * Return: the length of str, or -1 if str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
+ * append_text_to_file - append text to the end of a file
+ * @filename: the name of the file to append to
+ * @text_content: the data to append to filename
+ *
+ * Return: Upon success, return 1. Otherwise, return -1.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fdopen, w;
+	ssize_t b_written = 0;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	fdopen = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fdopen == -1)
+	if (fd < 0)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
 
-	w = write(fdopen, text_content, strlen(text_content));
-	if (w == -1)
+	close(fd);
+
+	if (b_written < 0)
 		return (-1);
 	return (1);
 }
