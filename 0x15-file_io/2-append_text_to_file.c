815c71cef087d195e4f5f7b3cf3ef14990f7e9f2
Muradamen <muradamen10@gmail.com>
2022-11-01 22:15:45 +0300

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,36 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stddef.h>
+#include <string.h>
 /**
- * append_text_to_file - appends text at the end of a file
- *
- * @filename: name of the file
- * @text_content: text to add to the end of the file
- *
- * Return: 1 on success, -1 on failure (NULL filename, bad permissions)
+ * append_text_to_file - Appends to a file
+ * @filename: Filename
+ * @text_content: String to write to file
+ * Return: 1 for success, -1 on failure
  */
 
-int	append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_WRONLY | O_APPEND), len = 0;
-	ssize_t write_o = 0;
+	int fdopen, w;
 
-	if (fd > -1 && filename)
-	{
-		while (text_content && text_content[len])
-			len++;
-		if (len > 0)
-			write_o = write(fd, text_content, len);
-		close(fd);
-		if (write_o > -1)
-			return (1);
-	}
-	return (-1);
+	if (filename == NULL)
+		return (-1);
+
+	fdopen = open(filename, O_RDWR | O_APPEND);
+
+	if (fdopen == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	w = write(fdopen, text_content, strlen(text_content));
+	if (w == -1)
+		return (-1);
+	return (1);
 }
