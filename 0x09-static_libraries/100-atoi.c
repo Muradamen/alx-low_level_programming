56aa46b06a13c21edcf1a6b507913f8aa6ebc739
Muradamen <muradamen10@gmail.com>
2022-11-03 23:48:52 +0300

    static libarary new

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi - convert to a int
+ * @s:string
+ * Return:int
+ */
+
+int _atoi(char *s)
+{
+int i, j, n, x;
+
+	i = n = 0;
+	x = 1;
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			x *= -1;
+		i++;
+	}
+	j = i;
+	while ((s[j] >= '0') && (s[j] <= '9'))
+	{
+		n = (n * 10) + x * ((s[j]) - '0');
+		j++;
+	}
+	return (n);
+}
