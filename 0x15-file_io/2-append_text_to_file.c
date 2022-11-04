0549ae93463cd676ae9b533b41c77ad4bd84ec64
Muradamen <muradamen10@gmail.com>
2022-11-04 22:20:20 +0300

    append tet to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
+ * main - check the code
  *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+ * Return: Always 0.
  */
-int append_text_to_file(const char *filename, char *text_content)
+int main(int ac, char **av)
 {
-	int fd;
-	int nletters;
-	int rwr;
+    int res;
 
-	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
-	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
-
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-			return (-1);
-	}
-
-	close(fd);
-
-	return (1);
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = append_text_to_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
-
