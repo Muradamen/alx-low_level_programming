334a4bb4215d87cc8a3ff8ecb767bd2a47cafc7d
Muradamen <muradamen10@gmail.com>
2022-12-15 00:45:50 -0800

    create dy

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c && gcc -shared -o liball.so *.o
+gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c
+gcc -shared -o libdynamic.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
